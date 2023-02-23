9a7350369b49343c6a05bdb4ca2d43489851a21f
Randy Chu <randychu7@gmail.com>
2023-02-23 15:26:25 -0600

    lol

---
+++ src/FileIo.java
@@ -0,0 +1,8 @@
+//import java.nio.file.Path;
+//
+//public class FileIo {
+//
+//    public static void main(String[] args) {
+//        Path myPath = Path.get("Directory","Filename");
+//    }
+//}
