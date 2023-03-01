dd686f1dc3ed78727e6a20183d42bede298c56a0
Randy Chu <randychu7@gmail.com>
2023-02-28 18:13:11 -0600

    lol

--- src/Shapes/Quadrilateral.java
+++ src/Shapes/Quadrilateral.java
@@ -3,6 +3,7 @@ package Shapes;
 public abstract class Quadrilateral extends Shape implements Measurable{
 
     protected int length;
+
     protected int width;
 
    public Quadrilateral(int length, int width){
